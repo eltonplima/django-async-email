e0df0c75f3ade489bb972a9ad344a8d51e65ec57
eltonplima <me@eltonplima.dev>
2020-10-13 21:34:24 +0100

    Add public send_email_template
    * This function send a message to the celery broker

--- src/async_email/__init__.py
+++ src/async_email/__init__.py
@@ -1,5 +1,29 @@
 __all__ = ["send_email_template"]
 
-from async_email.email import send_email_template
+from typing import Dict
+from typing import Tuple
+
+from celery import Celery
+from celery.result import AsyncResult
+from django.conf import settings
+
 
 default_app_config = "async_email.apps.AsyncEmailConfig"
+
+
+def send_email_template(
+    to: Tuple[str], template_name: str, context: Dict, from_email: str = None
+) -> AsyncResult:
+    app = Celery("async_email", broker=settings.CELERY_BROKER_URL, backend="amqp")
+    result = app.send_task(
+        template_name,
+        queue=template_name,
+        kwargs={
+            "template_name": template_name,
+            "context": context,
+            "to": to,
+            "from_email": from_email or settings.DEFAULT_FROM_EMAIL,
+        },
+    )
+    return result
+
