ac0c68810713030763b00538780527eabf2c208b
eltonplima <me@eltonplima.dev>
2020-10-13 19:10:41 +0100

    Update function validate_email_address
    * Use the django validator validate_email instead of reinvent the wheel
    
    Signed-off-by: eltonplima <me@eltonplima.dev>

--- src/async_email/email/utils.py
+++ src/async_email/email/utils.py
@@ -1,8 +1,13 @@
-import re
 from email.utils import parseaddr
 
-from async_email.email.exceptions import EmailDomainNotFound, InvalidEmailAddress
-from dns.resolver import NXDOMAIN, Answer, query
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from dns.resolver import NXDOMAIN
+from dns.resolver import Answer
+from dns.resolver import query
+
+from async_email.email.exceptions import EmailDomainNotFound
+from async_email.email.exceptions import InvalidEmailAddress
 
 
 def resolve_dns_mx_record(fqdn: str) -> Answer:
@@ -17,12 +22,13 @@ def resolve_dns_mx_record(fqdn: str) -> Answer:
 
 
 def validate_email_address(email: str, validate_existence_of_mx_record=False):
-    expression = re.compile(r"\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b")
     _, extracted_email = parseaddr(email)
-    fqdn = extracted_email.rsplit("@", 1)[-1]
 
-    if not expression.match(extracted_email):
+    try:
+        validate_email(extracted_email)
+    except ValidationError:
         raise InvalidEmailAddress(f"The email address is invalid: {extracted_email}")
 
     if validate_existence_of_mx_record:
+        fqdn = extracted_email.rsplit("@", 1)[-1]
         resolve_dns_mx_record(fqdn)
