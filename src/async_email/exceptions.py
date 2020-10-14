1334fe0ce8d0f0d55e156db7782bda24c2881090
eltonplima <me@eltonplima.dev>
2020-10-14 19:48:26 +0100

    Remove unnecessary module and test
    Signed-off-by: eltonplima <me@eltonplima.dev>

--- src/async_email/exceptions.py
+++
@@ -1,2 +0,0 @@
-class MissingRequiredConfiguration(Exception):
-    pass
--- tests/test_async_email_initialization.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from async_email.exceptions import MissingRequiredConfiguration
-
-
-def test_settings_without_default_from_email(context, settings):
-    with pytest.raises(MissingRequiredConfiguration):
-        settings.DEFAULT_FROM_EMAIL = None
-        import async_email  # noqa
